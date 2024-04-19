8a29cc7b5e037162277702a004a4a620424fdaf3
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2024-04-19 10:01:38 -0700

    Update app.py

--- streamlit_app/app.py
+++ streamlit_app/app.py
@@ -1,39 +1,28 @@
 from st_pages import Page, show_pages, add_page_title  # allow multipages
 import streamlit as st
 import os, sys
-
+from glob import glob
 
 # ================================  add paths ================================ 
 parent_dir = os.path.dirname( os.path.realpath(__file__) )
 gparent_dir = os.path.dirname( parent_dir )
 
-st.text( parent_dir )
+st.text( parent_dir  )
 st.text( gparent_dir )
-
-sys.path.append(gparent_dir)
-sys.path.append(parent_dir)
+sys.path.append( gparent_dir )
+sys.path.append( parent_dir  )
 
 path_to_html = gparent_dir + '/visualizations/html/yearly_network_graph.html'
-
 with open(path_to_html,'r') as f: 
     html_data = f.read()
-
-# ================================  widget ================================ 
-# Show in webpage
-st.header("")
+    
+# ================================  Widget ================================ 
+st.header("Project 2")
 st.components.v1.html(html_data, scrolling=True, height=500 ) 
-
-
-# ================================ Widgets =============================== 
-
-from glob import glob
-files = glob( gparent_dir + '/visualizations/html/*.html' )
-pages = []
-for i,f in enumerate( files ):
-  pages.append( Page( f, f'Result {i}' ) ) 
-  
+ 
 show_pages(
     [
         Page( parent_dir + "/app.py", "Home", "🏠"),         
-    ] + pages
+        Page( parent_dir + "/other.py", "Other results"),         
+    ] 
 )
