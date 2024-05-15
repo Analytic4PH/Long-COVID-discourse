01cb25b50da01290fd7ccc7329b3690ab648ba15
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2024-05-15 14:02:58 -0700

    Update misc.py

--- streamlit_app/misc.py
+++ streamlit_app/misc.py
@@ -15,18 +15,21 @@ st.text( parent_dir  )
 st.text( gparent_dir )
 sys.path.append( gparent_dir )
 sys.path.append( parent_dir  )
- 
+
+st.write( 'This page is only deployed for demo purposes.')
+
 # ================================  Widget ================================ 
 path_to_html = gparent_dir + '/visualizations/project2/yearly_network_graph.html'
 with open(path_to_html,'r') as f: 
     html_data = f.read()
 
 st.header("Project 2")
+st.write( 'Below show rendering of notebooks (which are far from usable)')
 components.html(html_data, scrolling=True, height=500 ) 
  
 # ================================ Widget =============================== 
 st.title("Notebooks")
-
+st.write( 'Below show rendering of notebooks')
 if 1:
   files = glob( gparent_dir + '/visualizations/notebooks/*.*' )
   html_datasets, pages = [], []
