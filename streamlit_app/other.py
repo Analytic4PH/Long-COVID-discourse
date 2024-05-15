72f2b0c8765814c9c4bdb1842013f8e939f83089
lisatwyw <38703113+lisatwyw@users.noreply.github.com>
2024-05-15 13:35:26 -0700

    Update other.py

--- streamlit_app/other.py
+++ streamlit_app/other.py
@@ -24,36 +24,37 @@ st.header("Other results")
 
 tabs = st.tabs( ['Europe', 'USA', 'Canada'] )
 
-with tabs[0]:
-  files = glob( gparent_dir + '/visualizations/html/EU/eu*.html' )
-  html_datasets, pages = [], []
-  st.header( 'Results on tweets from Europe' )
-  for i,file in enumerate( files ):
-    with open(file,'r') as f: 
-      html_data = f.read()   
-    st.write( file )
-    components.html(html_data, scrolling=True, height=1000 ) 
-
-with tabs[1]:
-  st.header( 'Results on tweets from USA' )
-  files = glob( gparent_dir + '/visualizations/html/US/us*.html' )
-  html_datasets, pages = [], []
-  for i,file in enumerate( files ):
-    with open(file,'r') as f: 
-      html_data = f.read()   
-    st.write( file )
-    components.html(html_data, scrolling=True, height=1000 ) 
-
-with tabs[2]:
-  st.header( 'Results on tweets from Canada' )
-  files = glob( gparent_dir + '/visualizations/html/*.html' )
-  html_datasets, pages = [], []
-  for i,file in enumerate( files ):
-    with open(file,'r') as f: 
-      html_data = f.read()   
-    st.write( file )
-    components.html(html_data, scrolling=True, height=1000 ) 
-
+for month in ['march', 'july', 'december' ]:
+ with tabs[0]:
+   files = glob( gparent_dir + f'/visualizations/html/EU/eu*{month}.html' )
+   html_datasets, pages = [], []
+   st.header( 'Results on tweets from Europe' )
+   for i,file in enumerate( files ):
+     with open(file,'r') as f: 
+       html_data = f.read()   
+     st.write( file )
+     components.html(html_data, scrolling=True, height=1000 ) 
+ 
+ with tabs[1]:
+   st.header( 'Results on tweets from USA' )
+   files = glob( gparent_dir + f'/visualizations/html/US/us*{month}.html' )
+   html_datasets, pages = [], []
+   for i,file in enumerate( files ):
+     with open(file,'r') as f: 
+       html_data = f.read()   
+     st.write( file )
+     components.html(html_data, scrolling=True, height=1000 ) 
+ 
+ with tabs[2]:
+   st.header( 'Results on tweets from Canada' )
+   files = glob( gparent_dir + f'/visualizations/html/*{month}.html' )
+   html_datasets, pages = [], []
+   for i,file in enumerate( files ):
+     with open(file,'r') as f: 
+       html_data = f.read()   
+     st.write( file )
+     components.html(html_data, scrolling=True, height=1000 ) 
+ 
 
 #html_datasets.append( html_data ) 
   
